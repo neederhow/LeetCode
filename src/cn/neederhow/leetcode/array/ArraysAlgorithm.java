209fd704292cd32ade1c8ccf3409f866c78940a8
倪德豪 <you@example.com>
2019-07-05 18:39:53 +0800

    至少是其他数字两倍的最大数

--- src/cn/neederhow/leetcode/array/ArraysAlgorithm.java
+++ src/cn/neederhow/leetcode/array/ArraysAlgorithm.java
@@ -16,7 +16,7 @@ public class ArraysAlgorithm {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      * @param nums * nums 的长度范围为 [0,10000]。
      *             * 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
-     * @return [-1,10000]
+     * @return [-1,9999]
      *
      * 使用Arrays.stream(nums).sum()，OJ耗时73ms
      * 使用foreach,OJ耗时4ms
@@ -26,7 +26,7 @@ public class ArraysAlgorithm {
     }
 
     // time: 74ms men: 46 MB
-    public int pivotIndex_v1(int[] nums) {
+    private int pivotIndex_v1(int[] nums) {
         int left = 0 , right = Arrays.stream(nums).sum();
         for (int i = 0; i < nums.length; i++) {
             if (i!=0) left += nums[i-1];
@@ -37,7 +37,7 @@ public class ArraysAlgorithm {
     }
 
     // time: 73ms men: 40 MB
-    public int pivotIndex_v2(int[] nums) {
+    private int pivotIndex_v2(int[] nums) {
         int left = 0 , sum = Arrays.stream(nums).sum();
         for (int i = 0; i < nums.length; i++) {
             if (left * 2 + nums[i] == sum ) return i;
@@ -47,7 +47,7 @@ public class ArraysAlgorithm {
     }
 
     // time: 4ms men: 37.9 MB
-    public int pivotIndex_v3(int[] nums) {
+    private int pivotIndex_v3(int[] nums) {
         int left = 0 , sum = 0;
         for (int i : nums) {
             sum += i;
@@ -57,5 +57,86 @@ public class ArraysAlgorithm {
             left += nums[i];
         }
         return -1;
+    }
+
+    /**
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     *
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+     *
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/plus-one
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param digits
+     * @returnx
+     */
+    public int[] plusOne(int[] digits){
+        return plusOne_v1(digits);
+    }
+
+    // time: 1 ms men: 35.8 MB
+    private int[] plusOne_v1(int[] digits) {
+        int n =digits.length;
+        for (int i = 1; i <= n; i++) {
+            if (digits[n-i]<9){
+                digits[n-i] = digits[n-i]+1;
+                return digits;
+            }
+            digits[n-i]=0;
+        }
+        int[] res = new int[n+1];
+        res[0]=1;
+        return res;
+    }
+
+    /**
+     *在一个给定的数组nums中，总是存在一个最大元素 。
+     *
+     * 查找数组中的最大元素是否至少是数组中每个其他数字的两倍。
+     *
+     * 如果是，则返回最大元素的索引，否则返回-1。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums * nums 的长度范围在[1, 50].
+     *             * 每个 nums[i] 的整数范围在 [0, 99].
+     * @return [-1, 49]
+     */
+    public int dominantIndex(int[] nums) {
+        return dominantIndex_v2(nums);
+    }
+
+    // time: 2 ms men: 35.5 MB
+    private int dominantIndex_v1(int[] nums) {
+        int index = 0,max = 0, submax = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>max){
+                submax = max;
+                max = nums[i];
+                index = i;
+                continue;
+            }
+            if (nums[i]>submax) submax = nums[i];
+        }
+        if (max >= submax * 2) return index;
+        return -1;
+    }
+
+    // time: 2 ms men: 35.3 MB
+    private int dominantIndex_v2(int[] nums){
+        int index = 0,max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max){
+                max = nums[i];
+                index = i;
+            }
+        }
+        for (int i = 0; i < nums.length; i++){
+            if (max < nums[i] * 2 && i != index) return -1;
+        }
+        return index;
     }
 }
--- src/cn/neederhow/leetcode/main/Main.java
+++ src/cn/neederhow/leetcode/main/Main.java
@@ -5,6 +5,7 @@ import cn.neederhow.leetcode.array.ArraysAlgorithm;
 public class Main {
     public static void main(String[] args) {
         ArraysAlgorithm a = new ArraysAlgorithm();
-        System.out.println(a.pivotIndex(new int[]{-1,-1,-1,-1,-1,-1}));
+        System.out.println(a.dominantIndex(new int[]{0,0,0,1}));
+//        System.out.println(a.pivotIndex(new int[]{-1,-1,-1,-1,-1,-1}));
     }
 }
