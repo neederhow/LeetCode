f54543643b58d2399fd2a863af73647bfb27b574
decret <decret@outlook.com>
2019-07-14 15:45:27 +0800

    #26删除排序数组中的重复项

--- src/cn/neederhow/leetcode/array/ArraysAlgorithm.java
+++ src/cn/neederhow/leetcode/array/ArraysAlgorithm.java
@@ -169,4 +169,28 @@ public class ArraysAlgorithm {
         res[0]=1;
         return res;
     }
+
+    /**
+     * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     *
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int removeDuplicates(int[] nums) {
+        return removeDuplicates_1(nums);
+    }
+    // time: 2 ms men: 43.9 MB
+    public int removeDuplicates(int[] nums) {
+        int sub = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[sub] != nums[i]){
+                sub++;
+                nums[sub] = nums[i];
+            }
+        }
+        return sub + 1 ;
+    }
 }
